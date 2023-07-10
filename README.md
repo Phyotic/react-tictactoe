ffda8e4eee3349d89f886e506dfbe6ea8087dfa7
Daniel Solorzano <dsolo.prof@gmail.com>
2023-07-10 11:12:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Tic-Tac-Toe web app built using React.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <p>React Tic-Tac-Toe App</p>
+        </>
+    );
+}
+
+export default App;
